a90265e9273f9cd37356fd234c6d968b5f292eb7
IonutVoicila <mariusvoicila03@yahoo.com>
2024-04-14 19:04:23 +0300

    Use generic CreateView for SignUp.

--- account/views.py
+++ account/views.py
@@ -1,20 +1,14 @@
 from django.http import HttpResponse
 from .forms import NewUserForm
-from django.views.generic.edit import FormView
 from django.contrib.auth.models import User
+from django.views.generic.edit import CreateView
+
 
 def index(request):
     return HttpResponse("Hello, world. You're at the account index.")
 
-
-class SignUpView(FormView):
-    """
-    Display the register form.
-    """
+class SignUpView(CreateView):
+    model = User
     form_class = NewUserForm
     template_name = 'account/register.html'
     success_url = '/account/login/'
-    
-    def form_valid(self, form):
-        form.save()
-        return super().form_valid(form)
