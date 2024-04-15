1409cb9a3152c376e053a3d8af91fea60e7cded9
Popa Sebastian <sebastianpopa208@gmail.com>
2024-04-15 13:32:05 +0300

    ignore

--- account/urls.py
+++ account/urls.py
@@ -1,13 +1,17 @@
-from django.urls import path
+from django.urls import path  # type: ignore
 
-from . import views
-from django.contrib import admin
-from django.contrib.auth import views as auth_views
+# from . import views
+# from django.contrib import admin
+from django.contrib.auth import views as auth_views  # type: ignore
 from .views import SignUpView, index
 
 urlpatterns = [
     path("", index, name="index"),
-    path('register/', SignUpView.as_view(), name='register'),
-	path('login/', auth_views.LoginView.as_view(template_name='account/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name = 'logout'),
+    path("register/", SignUpView.as_view(), name="register"),
+    path(
+        "login/",
+        auth_views.LoginView.as_view(template_name="account/login.html"),
+        name="login",
+    ),
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
 ]
