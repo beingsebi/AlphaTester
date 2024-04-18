54eaade7521fd83447c84222e01690986af596ec
Popa Sebastian <sebastianpopa208@gmail.com>
2024-04-19 00:23:04 +0300

    add indicator args to signal

--- utils/strategy/signal.py
+++ utils/strategy/signal.py
@@ -1,7 +1,8 @@
 class Signal:
-    def __init__(self, indicator: str, value: float, operator: str) -> None:
+    def __init__(self, indicator: str, value: float, operator: str, **indicatorArgs) -> None:
         # here will also check if indicator is valid
         self.indicator = indicator
         self.value = value
         # value might become Amount type if necessary
         self.operator = operator  # "<=" or ">="
+        self.indicatorArgs = indicatorArgs
