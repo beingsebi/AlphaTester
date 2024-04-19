2a9a7aa064ec6d8c1b4253add8a8fdc15d0268ec
Popa Sebastian <sebastianpopa208@gmail.com>
2024-04-19 12:27:07 +0300

    name fix

--- utils/strategy/signal.py
+++ utils/strategy/signal.py
@@ -1,7 +1,7 @@
 class Signal:
-    def __init__(self, indicator: str, value: float, operator: str, **indicatorArgs) -> None:
+    def __init__(self, indicatorName: str, value: float, operator: str, **indicatorArgs) -> None:
         # here will also check if indicator is valid
-        self.indicator = indicator
+        self.indicator = indicatorName
         self.value = value
         # value might become Amount type if necessary
         self.operator = operator  # "<=" or ">="
