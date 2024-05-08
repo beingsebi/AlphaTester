3f744e93d9546ee554f79a69f00f6ebaf0bf4a38
IonutVoicila <mariusvoicila03@yahoo.com>
2024-05-08 19:08:55 +0300

    Add a base html for templates. Small work to improve the app frontend, now there is a base that all templates use. Improve the Strategy detail view by outputing all of the details from details Create a minimal navbar that has Account Home and Register/Login/Logout buttons. Created a file for css and used Bootstrap.

--- account/urls.py
+++ account/urls.py
@@ -6,12 +6,12 @@ from django.urls import path  # type: ignore
 from .views import SignUpView, index
 
 urlpatterns = [
-    path("", index, name="index"),
     path("register/", SignUpView.as_view(), name="register"),
     path(
         "login/",
-        auth_views.LoginView.as_view(template_name="account/login.html"),
-        name="login",
+        auth_views.LoginView.as_view(template_name="account/login.html", next_page="/"),
+        name="login", 
     ),
+    # TODO: Add a custom logout template view.
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
 ]
