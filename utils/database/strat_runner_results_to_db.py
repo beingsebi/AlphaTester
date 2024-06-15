78765ec9d427522d142c1986be800c5366b3631e
IonutVoicila <mariusvoicila03@yahoo.com>
2024-06-15 19:04:24 +0300

    Use the pk to the task becasue the celery doesn't know how to parse the Strategy object.

--- utils/database/strat_runner_results_to_db.py
+++ utils/database/strat_runner_results_to_db.py
@@ -1,13 +1,18 @@
 from typing import List
+import jsonpickle
 from backtester.models import Strategy
 from utils.strategy.strategy import StrategyDetails
 from utils.strategyRunner.resultsInterpretor import Results, ResultsInterpretor
 from utils.strategyRunner.strategyRunner import StrategyRunner, Transaction
 
 
-def update_results(strategy: Strategy):
+def update_results(strategy_id):
+    strategy = Strategy.objects.get(pk=strategy_id)
+
     strat = StrategyDetails.fromJSON(strategy.strategyDetails)
+
     transactions: List[Transaction] = StrategyRunner.run(strat)
+
     results: Results = ResultsInterpretor.interpretResults(
         transactions, strat.capitalAllocation, strat.startDatetime
     )
